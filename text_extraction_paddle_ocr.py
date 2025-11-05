7589120800cc5420221d7be7c48099124071fab6
DexterMorganAlpha <dextermorganrohanarora@gmail.com>
2025-11-05 09:59:26 +0530

    updated text extraction scripts for paddle ocr.

--- text_extraction_paddle_ocr.py
+++ text_extraction_paddle_ocr.py
@@ -172,7 +172,7 @@ def get_input_video(reel_number):
             return input_video_path
 
 def process_all_videos():
-    for reel_number in range(1, 750):
+    for reel_number in range(361, 894):
         print(f"Processing reel number: {reel_number}")
         input_video_path = get_input_video(reel_number)
         process_video(input_video_path, reel_number)
